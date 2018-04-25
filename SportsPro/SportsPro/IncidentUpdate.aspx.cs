5af9e7f1ddb575998a905cf725518556332b4166
RYANH-PC\Ryan <rahollinger@my.waketech.edu>
2018-04-24 20:17:00 -0400

    Implements SelectMethod for customer dropdown

--- SportsPro/SportsPro/IncidentUpdate.aspx.cs
+++ SportsPro/SportsPro/IncidentUpdate.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using SportsPro.Models;
 
 namespace SportsPro {
     public partial class IncidentUpdate : System.Web.UI.Page {
@@ -11,8 +12,11 @@ namespace SportsPro {
 
         }
 
-        public IQueryable<SportsPro.Models.Customer> ddlCustomers_GetData() {
-            return null;
+        public IQueryable<Customer> ddlCustomers_GetData() {
+            TechSupportEntities db = new TechSupportEntities();
+            return from c in db.Customers
+                   orderby c.Name
+                   select c;
         }
     }
 }
